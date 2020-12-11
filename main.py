ae0b92d13434b43af0164440ef22bbc108ef9234
Doeke <doekebuursma@gmail.com>
2020-12-11 11:58:13 -0500

    Change icon

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ logger = logging.getLogger(__name__)
 
 
 class DemoExtension(Extension):
-
     def __init__(self):
         super(DemoExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
@@ -21,29 +20,37 @@ class DemoExtension(Extension):
 
 
 class KeywordQueryEventListener(EventListener):
-
     def on_event(self, event, extension):
         items = []
-        logger.info('preferences %s' % json.dumps(extension.preferences))
+        logger.info("preferences %s" % json.dumps(extension.preferences))
         for i in range(5):
-            item_name = extension.preferences['item_name']
-            data = {'new_name': '%s %s was clicked' % (item_name, i)}
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name='%s %s' % (item_name, i),
-                                             description='Item description %s' % i,
-                                             on_enter=ExtensionCustomAction(data, keep_app_open=True)))
+            item_name = extension.preferences["item_name"]
+            data = {"new_name": "%s %s was clicked" % (item_name, i)}
+            items.append(
+                ExtensionResultItem(
+                    icon="images/icon.svg",
+                    name="%s %s" % (item_name, i),
+                    description="Item description %s" % i,
+                    on_enter=ExtensionCustomAction(data, keep_app_open=True),
+                )
+            )
 
         return RenderResultListAction(items)
 
 
 class ItemEnterEventListener(EventListener):
-
     def on_event(self, event, extension):
         data = event.get_data()
-        return RenderResultListAction([ExtensionResultItem(icon='images/icon.png',
-                                                           name=data['new_name'],
-                                                           on_enter=HideWindowAction())])
+        return RenderResultListAction(
+            [
+                ExtensionResultItem(
+                    icon="images/icon.svg",
+                    name=data["new_name"],
+                    on_enter=HideWindowAction(),
+                )
+            ]
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     DemoExtension().run()
