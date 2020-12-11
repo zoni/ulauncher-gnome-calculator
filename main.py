886f4d4b66b798aa17e2d05990e0fa358f73034a
Doeke <doekebuursma@gmail.com>
2020-12-11 13:35:38 -0500

    Copy result to clipboard

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 import subprocess
 
 logger = logging.getLogger(__name__)
@@ -17,41 +18,25 @@ class DemoExtension(Extension):
     def __init__(self):
         super(DemoExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
 
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         query = event.get_argument()
         logger.info("preferences %s" % json.dumps(extension.preferences))
-        item_name = subprocess.run(
+        result = subprocess.run(
             ["gnome-calculator", "-s", query], stdout=subprocess.PIPE, text=True
         ).stdout
-        data = {"new_name": "%s was clicked" % item_name}
         items = [
             ExtensionResultItem(
                 icon="images/icon.svg",
-                name=item_name,
-                description="Item description",
-                on_enter=ExtensionCustomAction(data, keep_app_open=True),
+                name=result,
+                description="Enter to copy to the clipboard",
+                on_enter=CopyToClipboardAction(result),
             )
         ]
 
         return RenderResultListAction(items)
-
-
-class ItemEnterEventListener(EventListener):
-    def on_event(self, event, extension):
-        data = event.get_data()
-        return RenderResultListAction(
-            [
-                ExtensionResultItem(
-                    icon="images/icon.svg",
-                    name=data["new_name"],
-                    on_enter=HideWindowAction(),
-                )
-            ]
-        )
 
 
 if __name__ == "__main__":
